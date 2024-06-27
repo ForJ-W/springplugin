30622aef9193ab169de48417380fd7e20fcf4890
wujing <2941211504@qq.com>
2024-06-27 17:12:51 +0800

    feat(core): 默认插件配置类

---
+++ core/src/main/java/org/springplugin/core/context/DefaultSpringPluginConfiguration.java
@@ -0,0 +1,10 @@
+package org.springplugin.core.context;
+
+/**
+ * 默认插件配置
+ *
+ * @author wujing
+ * @version 1.0.0
+ */
+public class DefaultSpringPluginConfiguration extends SpringPluginConfiguration{
+}
